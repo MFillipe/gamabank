08d21b9152aef78df425e6711e3ac0aa18e1985d
Matheus Filipe <matheus_filipe00@outlook.com>
2021-04-03 12:46:06 -0300

    feat: create home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HomeComponent } from './components/home/home.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+import { CreateAccountFormComponent } from './components/create-account-form/create-account-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HomeComponent,
+    CreateAccountComponent,
+    CreateAccountFormComponent
   ],
   imports: [
     BrowserModule,
